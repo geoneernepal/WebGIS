68f45a4b009ee80aa6127aaf408629c2b5ed94d1
lostb1d <mylife.dinesh08@gmail.com>
2022-03-17 07:57:28 +0545

    first commit

---
+++ static/js/map.js
@@ -0,0 +1,23 @@
+var map = L.map('map', {zoomControl: false}).setView([51.505, -0.09], 13);
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibG9zdGIxIiwiYSI6ImNqaTBjcGd4bjE2cGMza3M2MWEzcTRwd3gifQ.Ps6yKHol2bmEPndMSeYKKw'
+}).addTo(map);
+
+L.control.zoom({
+    position: 'topright'
+}).addTo(map);
+
+
+
+
+
+
+
+
