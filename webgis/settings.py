a7156bc3c6de28f0271a0286aacb95a7a40ee435
geoneernepal <geoneernepal@gmail.com>
2022-06-27 16:34:32 +0545

    first commit

--- webgis/settings.py
+++ webgis/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gis'
+    'gis',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,4 @@ MEDIA_URL = './images/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- webgis/urls.py
+++ webgis/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gis.urls'))
+    path('', include('gis.urls')),
+    path('api/', include('api.urls')),
+
 ]
